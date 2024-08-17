9bf2c429870d5ffb4dcf7ca56777b4874d59a5a1
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-08-17 16:43:45 +0300

    Create exam1.js

---
+++ exam1.js
@@ -0,0 +1,22 @@
+function trip(input) {
+
+   let numberOfPeople = Number(input[0]);
+   let numberOfNights = Number(input[1]);
+   let numberOfTransportCards = Number(input[2]);
+   let numberOfTicketsMuseim = Number(input[3]);
+
+   let priceForNights = 20;
+   let priceForTransport = 1.60;
+   let priceForTicketsMuseim = 6;
+
+   let totalPriceForNights = priceForNights * numberOfPeople * numberOfNights;
+   let totalPriceTransport = priceForTransport * numberOfPeople * numberOfTransportCards;
+   let totalPriceMuseim = priceForTicketsMuseim * numberOfPeople * numberOfTicketsMuseim;
+
+   let allPrice = (totalPriceForNights + totalPriceMuseim + totalPriceTransport) * 1.25;
+
+   console.log(allPrice.toFixed(2));
+
+}
+
+trip(['131', '9', '33', '46'])
