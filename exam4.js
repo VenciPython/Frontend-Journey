18d5a691d143fc952967fe9350101d33e08818f2
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-08-17 18:53:04 +0300

    Update exam4.js

--- exam4.js
+++ exam4.js
@@ -2,48 +2,50 @@ function exam(input) {
     let numberOfStudents = Number(input[0]); // броят на студентите
     let grades = []; // масив за оценките
 
-    // Обхождаме стринга с оценките и събираме числата
+    // Парсване на стринга с оценките и събиране на числата в масива
     let currentGrade = '';
     for (let i = 0; i < input[1].length; i++) {
         if (input[1][i] !== ',') {
-            currentGrade += input[1][i]; // добавяме символа към текущото число
+            currentGrade += input[1][i]; // добавяме цифрата към текущото число
         } else {
-            grades.push(Number(currentGrade)); // добавяме текущото число в масива
+            grades.push(Number(currentGrade)); // добавяме числото в масива
             currentGrade = ''; // нулираме текущото число
         }
     }
-    // Добавяме последното число (след последната запетая)
+    // Добавяме последната оценка (след последната запетая)
     grades.push(Number(currentGrade));
 
+    // Инициализация на броячи за всяка група
     let topStudents = 0;
     let betweenFourAndFive = 0;
     let betweenThreeAndFour = 0;
     let fail = 0;
     let totalSum = 0;
 
+    // Преброяване на студентите в различни групи и изчисляване на общата сума на оценките
     for (let studentIndex = 0; studentIndex < numberOfStudents; studentIndex++) {
         let grade = grades[studentIndex];
         totalSum += grade;
 
         if (grade >= 5.00) {
             topStudents++;
-        } else if (grade >= 4.00) {
+        } else if (grade >= 4.00 && grade <= 4.99) {
             betweenFourAndFive++;
-        } else if (grade >= 3.00) {
+        } else if (grade >= 3.00 && grade <= 3.99) {
             betweenThreeAndFour++;
         } else {
             fail++;
         }
     }
 
-    // Изчисляване на процентите
+    // Изчисляване на процентите за всяка група
     let topStudentsPercent = (topStudents / numberOfStudents * 100).toFixed(2);
     let betweenFourAndFivePercent = (betweenFourAndFive / numberOfStudents * 100).toFixed(2);
     let betweenThreeAndFourPercent = (betweenThreeAndFour / numberOfStudents * 100).toFixed(2);
     let failPercent = (fail / numberOfStudents * 100).toFixed(2);
 
     // Изчисляване на средния успех
-    let averageGrade = (totalSum / numberOfStudents).toFixed(2);
+    let averageGrade = (totalSum / numberOfStudents).toFixed(3);
 
     // Извеждане на резултатите
     console.log(`Top students: ${topStudentsPercent}%`);
