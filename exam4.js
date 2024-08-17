0c8f10cfcb7ead5b7703b596d0de4acc46bedfc7
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-08-17 18:45:31 +0300

    Update exam4.js

--- exam4.js
+++ exam4.js
@@ -1,6 +1,19 @@
 function exam(input) {
-    let numberOfStudents = input[0]; // броят на студентите
-    let grades = input.slice(1); // оценките на студентите
+    let numberOfStudents = Number(input[0]); // броят на студентите
+    let grades = []; // масив за оценките
+
+    // Обхождаме стринга с оценките и събираме числата
+    let currentGrade = '';
+    for (let i = 0; i < input[1].length; i++) {
+        if (input[1][i] !== ',') {
+            currentGrade += input[1][i]; // добавяме символа към текущото число
+        } else {
+            grades.push(Number(currentGrade)); // добавяме текущото число в масива
+            currentGrade = ''; // нулираме текущото число
+        }
+    }
+    // Добавяме последното число (след последната запетая)
+    grades.push(Number(currentGrade));
 
     let topStudents = 0;
     let betweenFourAndFive = 0;
@@ -41,4 +54,4 @@ function exam(input) {
 }
 
 // Пример за тестови данни
-exam([10, 3.00, 2.99, 5.68, 3.01, 4, 4, 6.00, 4.50, 2.44, 5.00]);
+exam([10, '3.00,2.99,5.68,3.01,4,4,6.00,4.50,2.44,5.00']);
