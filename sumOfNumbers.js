3b2cf010b30a2c5152839192ea2ee93e7e4a36cb
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-08-18 15:09:21 +0300

    sumOfNumbers.js

---
+++ sumOfNumbers.js
@@ -0,0 +1,12 @@
+function sumOfNumbers(input) {
+    let numAsStr = input[0];
+    let sum = 0;
+
+    for (let i = 0; i < numAsStr.length; i++) {
+        let numAsNum = Number(numAsStr[i]); //преобразувам стринга числа в реални числа
+        //console.log(numAsNum); принтирам отделно всяка цифра за проверка
+        sum += numAsNum; // събира сбора на всички цифри
+    }
+    console.log(sum);
+}
+sumOfNumbers([`2345`])])
