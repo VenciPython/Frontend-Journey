804014b1c77062928ed1129534a03b0d942e0b53
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-08-20 17:36:17 +0300

    exam6.js

--- exam6.js
+++ exam6.js
@@ -19,3 +19,52 @@ function printMultiplicationTable(number) {
 // Примерен вход
 let input = 231;
 printMultiplicationTable(input);
+
+
+
+
+
+
+-------------
+function sumAndProduct(input) {
+    let n = Number(input[0]);
+    //   let bufer = 0;
+    // let m = 0;
+    let isAbcd = false;
+    let isDcba = false;
+ 
+    for (let a = 1; a <= 9; a++) {
+        for (let b = a; b <= 9; b++) {
+            for (let c = 0; c <= 9; c++) {
+                for (let d = c; d <= 9; d++) {
+ 
+                    let sum = a + b + c + d;
+                    let box = a * b * c * d;
+                 
+                    // if (box === 0) {
+                    //     break;
+                    // }
+                    let del = box / sum;
+ 
+ 
+                    if (sum === box && n % 10 === 5) {
+                        isAbcd = true;
+                        break;
+                    } else if ((del) === 3 && n % 3 === 0) {
+                        isDcba = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    if (isAbcd) {
+        console.log(`abcd - ${a}${b}${c}${d}`);
+    } else if (isDcba) {
+        console.log(`dcba - ${d}${c}${b}${a}`);
+    } else {
+        console.log("Nothing found");
+ 
+    }
+}
+sumAndProduct(["123"]);
