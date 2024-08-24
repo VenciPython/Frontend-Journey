58db948adac052af57242dd866e40e42817a1794
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-08-24 15:59:22 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,35 @@
-function calculateProcessors(input) {
-    let plannedProcessors = Number(input[0]);
-    let workers = Number(input[1]);
-    let workingDays = Number(input[2]);
+function calculateCost(input) {
+    let month = input[0];
+    let hours = Number(input[1]);
+    let people = Number(input[2]);
+    let timeOfDay = input[3];
 
-    let hoursAvailable = workers * workingDays * 8;
-    let processorsProduced = Math.floor(hoursAvailable / 3);
-    let processorPrice = 110.10;
+    let pricePerHour = 0;
 
-    if (processorsProduced >= plannedProcessors) {
-        let profit = (processorsProduced - plannedProcessors) * processorPrice;
-        console.log(`Profit: -> ${profit.toFixed(2)} BGN`);
-    } else {
-        let loss = (plannedProcessors - processorsProduced) * processorPrice;
-        console.log(`Losses: -> ${loss.toFixed(2)} BGN`);
+    if (month === 'march' || month === 'april' || month === 'may') {
+        if (timeOfDay === 'day') {
+            pricePerHour = 10.50;
+        } else {
+            pricePerHour = 8.40;
+        }
+    } else if (month === 'june' || month === 'july' || month === 'august') {
+        if (timeOfDay === 'day') {
+            pricePerHour = 12.60;
+        } else {
+            pricePerHour = 10.20;
+        }
     }
+
+    if (people >= 4) {
+        pricePerHour *= 0.90;
+    }
+
+    if (hours >= 5) {
+        pricePerHour *= 0.50;
+    }
+
+    let totalCost = pricePerHour * hours * people;
+
+    console.log(`Price per person for one hour: ${pricePerHour.toFixed(2)}`);
+    console.log(`Total cost of the visit: ${totalCost.toFixed(2)}`);
 }
