363e8120c1d64edca8963c0dca5a0698f8159315
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-08-24 17:39:32 +0300

    christmasGifts.js

---
+++ christmasGifts.js
@@ -0,0 +1,25 @@
+function christmasGifts(input) {
+    let adultsCount = 0;
+    let kidsCount = 0;
+    let moneyForToys = 0;
+    let moneyForSweaters = 0;
+    let index = 0;
+
+    while (input[index] !== "Christmas") {
+        let age = Number(input[index]);
+
+        if (age <= 16) {
+            kidsCount++;
+            moneyForToys += 5;
+        } else {
+            adultsCount++;
+            moneyForSweaters += 15;
+        }
+        index++;
+    }
+    console.log(`Number of adults: ${adultsCount}`);
+    console.log(`Number of kids: ${kidsCount}`);
+    console.log(`Money for toys: ${moneyForToys}`);
+    console.log(`Money for sweaters: ${moneyForSweaters}`);
+}
+//christmasGifts(['18','20','48','45','56','37','12','14','Christmas'])
