3b815d8657a46702ed4ce93df9703db7c8631f2c
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-08-24 15:53:24 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
-let pagePrice = parseFloat(prompt("Enter the price for printing one page (0.00...1.50):"));
-let coverPrice = parseFloat(prompt("Enter the price for printing one cover (0.00...5.00):"));
-let discountPercentage = parseInt(prompt("Enter the discount percentage for printing (0...100):"));
-let designerFee = parseFloat(prompt("Enter the fee to be paid to the designer (0.00...150.00):"));
-let teamPercentage = parseInt(prompt("Enter the percentage of the total amount covered by the team (0...100):"));
+function calculateProcessors(input) {
+    let plannedProcessors = Number(input[0]);
+    let workers = Number(input[1]);
+    let workingDays = Number(input[2]);
 
-const numberOfPages = 899;
-const numberOfCovers = 2;
+    let hoursAvailable = workers * workingDays * 8;
+    let processorsProduced = Math.floor(hoursAvailable / 3);
+    let processorPrice = 110.10;
 
-let totalPageCost = pagePrice * numberOfPages;
-let totalCoverCost = coverPrice * numberOfCovers;
-let totalPrintingCost = totalPageCost + totalCoverCost;
-let discountedPrintingCost = totalPrintingCost * (1 - discountPercentage / 100);
-let totalCost = discountedPrintingCost + designerFee;
-let amountToPay = totalCost * (1 - teamPercentage / 100);
-
-console.log(`Avtonom should pay ${amountToPay.toFixed(2)} BGN.`);
+    if (processorsProduced >= plannedProcessors) {
+        let profit = (processorsProduced - plannedProcessors) * processorPrice;
+        console.log(`Profit: -> ${profit.toFixed(2)} BGN`);
+    } else {
+        let loss = (plannedProcessors - processorsProduced) * processorPrice;
+        console.log(`Losses: -> ${loss.toFixed(2)} BGN`);
+    }
+}
