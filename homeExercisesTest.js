42ea113f7ada356c6d55640a8659b674c2dec2c9
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-09-15 14:51:18 +0300

    homeExercisesTest.js

--- homeExercisesTest.js
+++ homeExercisesTest.js
@@ -47,3 +47,20 @@ console.log(JSON.stringify(car, null,2));
 // }
 
 // object()
+
+---------------------------------------------------
+
+//5
+function objectPerson() {
+
+    let person = {
+        name: `John`,
+        age: 30,
+        greet: function () {
+            console.log(`Hello, my name is ${this.name}, and I am ${this.age} years old`);
+
+        }
+    }
+    person.greet()
+}
+objectPerson()
