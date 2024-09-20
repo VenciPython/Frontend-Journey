315ec9f78d88a8cd271a866ca7ad27e2d63a374f
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-09-20 22:47:56 +0300

    rename to basketballEquipment.js

--- basketballEquipment.js
+++ basketballEquipment.js
@@ -1,7 +1,6 @@
 function basketballEquipment(input) {
 
   let oneYearPrice = Number(input[0]);
-
   let shoes = (40 / 100) * oneYearPrice;
   //вместо тази променлива може директно 100 - 40 = 60% от годишната такса значи ще го запишем така: 0.6 * oneYearPrice
 
@@ -9,7 +8,6 @@ function basketballEquipment(input) {
   //и ще имам само една променлива -  priceShoes = 0.6 * oneYearPrice
 
   let basketballShirt = (20 / 100) * priceShoes;
-
   let priceBasketballShirt = priceShoes - basketballShirt;
   // както и тук 80% от цената на обувките значи ще имам само една променлива - basketballShirt = 0.8 * priceShoes
 
@@ -20,5 +18,4 @@ function basketballEquipment(input) {
   console.log(allSum);
 
 }
-
 //basketballEquipment(["550"])
