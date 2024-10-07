8f33cf33ae6d0c7b008bb3f2dcd1e8b5960c412e
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-10-07 16:57:53 +0300

    Create fishland.js
    Need to complete this task!

---
+++ fishland.js
@@ -0,0 +1,10 @@
+function fishland(){
+  let skumPriceKilo = Number(input[0]);
+  let tsatsaPriceKilo = Number(input[1]);
+  let palamoudBuyKg = Number(input[2]);
+  let palamoudBuyKg = Number(input[3]);
+  let palamoudPriceKilo = (skumPriceKilo * 0.60) + skumPriceKilo;
+  let safridPriceKilo = (tsatsaPriceKilo * 0.80) + tsatsaPriceKilo;
+  let midiPriceKilo = 7.50;
+}
+fishlandL([])
