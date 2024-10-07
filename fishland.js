e7eabc0173c84026cc220f296c0d9e982c6ecc82
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-10-07 22:57:42 +0300

    Update fishland.js
    completed

--- fishland.js
+++ fishland.js
@@ -1,10 +1,19 @@
-function fishland(){
-  let skumPriceKilo = Number(input[0]);
-  let tsatsaPriceKilo = Number(input[1]);
-  let palamoudBuyKg = Number(input[2]);
-  let palamoudBuyKg = Number(input[3]);
-  let palamoudPriceKilo = (skumPriceKilo * 0.60) + skumPriceKilo;
-  let safridPriceKilo = (tsatsaPriceKilo * 0.80) + tsatsaPriceKilo;
-  let midiPriceKilo = 7.50;
+function fishland(input) {
+    let skumPriceKilo = Number(input[0]);
+    let tsatsaPriceKilo = Number(input[1]);
+    let palamoudBuyKg = Number(input[2]);
+    let safridBuyKg = Number(input[3]);
+    let midiBuyKg = Number(input[4]);
+
+    let palamoudPriceKilo = (skumPriceKilo * 0.60) + skumPriceKilo;
+    let safridPriceKilo = (tsatsaPriceKilo * 0.80) + tsatsaPriceKilo;
+    let midiPriceKilo = 7.50;
+
+    let palamoudAllSum = palamoudBuyKg * palamoudPriceKilo;
+    let safridAllSum = safridBuyKg * safridPriceKilo;
+    let midiAllSum = midiPriceKilo * midiBuyKg;
+
+    let allSum = palamoudAllSum + safridAllSum + midiAllSum;
+    console.log(allSum.toFixed(2))
 }
-fishlandL([])
+//fishland(['6.90', '4.20', '1.5', '2.5', '1'])
