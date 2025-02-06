879b131a6049fe09c7e1653d51ffbdf406e24bab
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2025-02-06 21:33:01 +0200

    Create apiKey.js

---
+++ apiKey.js
@@ -0,0 +1,29 @@
+{
+  "launch": {
+    "configurations": [
+      {
+        "type": "node",
+        "request": "launch",
+        "name": "Launch Program",
+        "program": "${file}"
+      }
+    ],
+    "compounds": []
+  },
+  "files.autoSave": "afterDelay",
+  "editor.minimap.enabled": false,
+  "editor.multiCursorModifier": "ctrlCmd",
+  "editor.formatOnSave": true,
+  "editor.mouseWheelZoom": true,
+  "json.schemas": [
+  
+  ],
+  "livePreview.notifyOnOpenLooseFile": false,
+  "livePreview.defaultPreviewPath": "/Users/lia-emilia/Documents/Venci_programming/chatGptTest/index.html",
+  "cSpell.ignoreWords": [
+    "VenciDev"
+  ],
+  "php.validate.executablePath": ""
+}
+// file ---> Preferenses ---> Settings  ---> Json (json: Schemas)
+//api key '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'
